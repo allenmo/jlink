a8ea699b707503eb98f775764437a77f1df8ea93
allenmo <allen02403@126.com>
2016-09-19 22:04:15 +0800

    update import

--- daemonDFW.py
+++ daemonDFW.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
-import subprocess
 import commands
+import subprocess
 import time
 import re
 
@@ -9,11 +9,12 @@ while True:
     pattern = re.compile('downloadFW_JTAG', re.S)
     match = re.search(pattern, output)
     if match:
-        print "program is running, no need to start"
+        #print "program is running, no need to start"
+        pass
     else:
-        print "program not run yet, now will start"
+        #print "program not run yet, now will start"
         subprocess.Popen(r'python downloadFW_JTAG.py', shell=True)
         time.sleep(5)
-    print status 
-    print output
+    #print status 
+    #print output
     time.sleep(5)
--- downloadFW_JTAG.py
+++ downloadFW_JTAG.py
@@ -2,7 +2,7 @@
 
 import RPi.GPIO as GPIO
 import time
-import subprocess
+#import subprocess
 import os
 import commands
 import re
