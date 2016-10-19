b35d2e2a28ddfdc630e705bc72708459a4d0fb63
allenmo <allen02403@gmail.com>
2016-10-19 16:25:07 +0800

    update check_network fun

--- fw/fw.py
+++ fw/fw.py
@@ -140,6 +140,13 @@ class Fw(object):
             print "ip:", ip
             p = subprocess.Popen(["ping -c 1 -w 1 " + ip], stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
             out = p.stdout.read()
+            err = p.stderr.read()
+            
+            regex = re.compile('unreachable')
+            if len(regex.findall(err)) >0:
+                print err
+                return "ERR"
+
             regex = re.compile('100% packet loss')
             if len(regex.findall(out)) == 0:
                 print ip + ': host up'
